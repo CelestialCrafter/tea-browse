824579ae60ae4c82966bba4ed65168937cc597eb
Ed Luff <beartechtalks@gmail.com>
2020-02-10 21:51:58 +0000

    move url styles

---
+++ src/components/styles/url.js
@@ -0,0 +1,20 @@
+const style = {
+	     light: {
+		           urlBar {
+                       backgroundColor: '#f3f3f3',
+                       margin: 10,
+                       marginTop: 1,
+                       width: '100%',
+                       height: '80%',
+                       borderRadius: 5,
+                       border: 'none',
+                       outline: 'none
+               },
+               urlBarContainer: { textAlign: 'center', flex: 1, display: 'flex' }
+       }
+};
+
+style.dark = style.light;
+
+module.exports = style.dark;
+module.exports = style.light;
